5dddcd86f8112e094ee3b4af2f997b73cb744e8b
Éder Amadeu <eder.amadeu@gmail.com>
2023-01-09 14:08:36 -0300

    commit atualizaJS

--- scritp.js
+++ scritp.js
@@ -1,8 +1,29 @@
-const circulo = document.querySelector("#circulo");
+/*const circulo = document.querySelector("#circulo");
 
 function seguirMouse(event) {
   circulo.style.top = event.y + "px";
   circulo.style.left = event.x + "px";
 }
 
-window.addEventListener("mousemove", seguirMouse);
+window.addEventListener("mousemove", seguirMouse);*/
+
+/*--------------------------------------------*/
+
+/*Exercício de botão*/
+
+const botao = document.querySelector("button");
+
+function somar() {
+  const div = document.querySelector("div");
+  const total = Number(div.innerText) + 1;
+
+  if (total <= 10) {
+    div.innerText = total;
+  } else {
+    div.innerText = "Impossível adicionar mais um";
+  }
+}
+
+if (botao) {
+  botao.addEventListener("click", somar);
+}
